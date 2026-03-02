a40d47f15f62448a39bbb99cecfe4332dcf4cdfa
dayoung <143780983+kdyann@users.noreply.github.com>
2026-03-02 16:46:05 +0900

    Title: 입력받은 값 교체하기, Time: 85ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260302/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int temp;
+        temp = b;
+        b = a;
+        a = temp;
+
+        System.out.println(a+" "+b);
+    }
+}
